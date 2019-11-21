4eeaa3ddc5404ab1edceb0a94a96842e5ec04194
ChiaraNebuloni <Administrator@192.168.12.60>
2019-11-21 16:14:19 +0100

    connection funzionante e nuovo info_it.properties e modifiche pom

--- progettocorsi/src/main/java/com/ats/utility/ConnectionFactory.java
+++ progettocorsi/src/main/java/com/ats/utility/ConnectionFactory.java
@@ -4,11 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.ResourceBundle;
 
+
 import com.ats.exception.DaoException;
 
 public class ConnectionFactory {
 	private static Connection conn;
-	private static ResourceBundle rb = ResourceBundle.getBundle("resources/info");
+	
+	private static ResourceBundle rb = ResourceBundle.getBundle("info_it");
+	
 	private static String stringConnection = rb.getString("jdbc.stringConnection");
 	private static String idConnection=rb.getString("jdbc.username");
 	private static String passConnection=rb.getString("jdbc.password");
