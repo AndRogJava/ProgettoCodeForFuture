a74d290cb6fdb5ecad1892d7e62eb7ea555698ef
SeleneAllevi <Administrator@192.168.12.61>
2019-11-21 10:01:56 +0100

    daoException connectionfactory info.properties
    da Selene il 21 nov 10.00

--- progettocorsi/src/main/java/com/ats/utility/ConnectionFactory.java
+++ progettocorsi/src/main/java/com/ats/utility/ConnectionFactory.java
@@ -1,6 +1,33 @@
 package com.ats.utility;
 
-public class ConnectionFactory {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.ResourceBundle;
 
+import com.ats.exception.DaoException;
+
+public class ConnectionFactory {
+	private static Connection conn;
+	private static ResourceBundle rb = ResourceBundle.getBundle("risorse/info");
+	private static String stringConnection = rb.getString("jdbc.stringConnection");
+	private static String idConnection=rb.getString("jdbc.username");
+	private static String passConnection=rb.getString("jdbc.password");
+	
+	
+	
+	private ConnectionFactory() throws DaoException{
+		
+		try {
+			conn = DriverManager.getConnection(stringConnection,idConnection,passConnection);
+		} catch (Exception e) {
+			throw new DaoException();
+		}
+	}
+	
+	public static Connection getInstance()throws DaoException{
+		if(conn==null)
+			new ConnectionFactory();
+		
+		return conn; 
+	}
 }
-aaghaua
