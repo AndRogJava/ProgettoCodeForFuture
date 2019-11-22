0e7b71cace1ff654d29d7a82867bf4f983f4f125
DanielaDamiano <Administrator@192.168.12.58>
2019-11-22 15:38:54 +0100

    ListaServlet giusta

--- progettocorsi/src/main/java/com/ats/controller/ListaServlet.java
+++ progettocorsi/src/main/java/com/ats/controller/ListaServlet.java
@@ -1,81 +1,14 @@
 package com.ats.controller;
 
 import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.LinkedList;
 
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.ats.exception.DaoException;
-import com.ats.model.DatiCorsisti;
-import com.ats.service.CorsistaService;
-
-
-@WebServlet("/ListaServlet")
-public class ListaServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-       
-    
-    public ListaServlet() {
-        super();
-        
-    }
-
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {
-//		String nomeIns=request.getParameter("Nomeadmin");
-//		int codIns=request.getParameter("Codadmin");
-		
-		HttpSession session = request.getSession();
-		RequestDispatcher rd=null;
-		
-		
-		LinkedList<DatiCorsisti> listaTutti=null;
-		try {
-			CorsistaService cs = new CorsistaService();
-			listaTutti = cs.selectAll();
-		} catch (DaoException e) {
-			session.setAttribute("erroreDao", "Error occurred during ListView");
-		  rd= request.getRequestDispatcher("PagError.jsp");
-			rd.forward(request, response);
-		}
-		
-		session.setAttribute("lista", listaTutti);
-		System.out.println("stampalista" + listaTutti);
-		rd=request.getRequestDispatcher("lista.jsp");
-		rd.forward(request, response);
-		}
-	
-
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-		doGet(request, response);
-	}
-
-}
-package com.ats.controller;
-
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.LinkedList;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.ats.exception.DaoException;
@@ -116,7 +49,7 @@ System.out.println("stampalista" + listaTutti);
 } catch (DaoException e) {
 e.printStackTrace();
 }
-rd=request.getRequestDispatcher("JSP/ProfiloCorsista.jsp");
+rd=request.getRequestDispatcher("JSP/lista.jsp");
 rd.forward(request, response);
 
 }
@@ -124,24 +57,6 @@ rd.forward(request, response);
 
 
 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/*
-HttpSession session = request.getSession();
-RequestDispatcher rd;
-
-
-LinkedList<DatiCorsisti> listaTutti=null;
-try {
-CorsistaService cs = new CorsistaService();
-listaTutti = cs.selectAll();
-session.setAttribute("lista", listaTutti);
-System.out.println("stampalista" + listaTutti);
-
-} catch (DaoException e) {
-}
-
-rd=request.getRequestDispatcher("ProfiloCorsista.jsp");
-rd.forward(request, response);
-}
-*/
+	doGet(request, response);
 }
 }
