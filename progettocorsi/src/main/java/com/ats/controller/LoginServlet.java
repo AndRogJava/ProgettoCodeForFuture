3310e333545dce095b5cdd00382e910c614c968b
RossellaDp <Administrator@192.168.12.50>
2019-11-25 14:27:23 +0100

    Login servlet, pronta e testata.

--- progettocorsi/src/main/java/com/ats/controller/LoginServlet.java
+++ progettocorsi/src/main/java/com/ats/controller/LoginServlet.java
@@ -1,5 +1,78 @@
 package com.ats.controller;
 
-public class LoginServlet {
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.ats.exception.DaoException;
+import com.ats.model.DatiAmministratori;
+import com.ats.service.DatiAmministratoriService;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public LoginServlet() {
+		super();
+
+	}
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		RequestDispatcher rd=null;
+		DatiAmministratoriService as=null;
+		try {
+			as = new DatiAmministratoriService();
+		} catch (DaoException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		DatiAmministratori amministratore = new DatiAmministratori();
+		String nomeIns = request.getParameter("nomeadmin");
+		int codice = Integer.parseInt(request.getParameter("codadmin"));
+	
+		
+		try {
+			amministratore = as.getAmministratorebyUsername(codice);
+			System.out.println("nome" + amministratore);
+			if (amministratore!=null  && amministratore.getCodadmin()==codice && amministratore.getNomeadmin().equalsIgnoreCase(nomeIns)) {
+				session.setAttribute("nomeadmin", nomeIns);		
+				session.setAttribute("codadmin", codice);
+				rd = request.getRequestDispatcher("JSP/lista.jsp");
+				rd.forward(request,response);
+
+			}else  {
+				String error = "Credenziali invalide!";
+				session.setAttribute ("error", error);
+				rd = request.getRequestDispatcher("index.jsp");
+				rd.forward(request,response);
+			}
+
+
+		} catch (DaoException e) {
+//			sql.printStackTrace(); gestione eccezioni per quando avremo la jsp dell'errore
+//			sql.getMessage();
+//			rd = request.getRequestDispatcher("Error.jsp");
+//			session.setAttribute("Errore","Siamo spiacenti, si è verificato un errore durante il login.");
+//			richiesta.forward(request, response);
+			e.printStackTrace();
+		}
+
+		
+		}
 
 }
