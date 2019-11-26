3fbb0f42dbc67b661d623784ad398b1f77c7b86a
RossellaDp <Administrator@192.168.12.50>
2019-11-26 14:41:26 +0100

    riprova

--- progettocorsi/src/main/java/com/ats/controller/LoginServlet.java
+++ progettocorsi/src/main/java/com/ats/controller/LoginServlet.java
@@ -33,8 +33,8 @@ public class LoginServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		RequestDispatcher rd=null;
-		DatiAmministratoriService as=null;
+		RequestDispatcher rd= null;
+		DatiAmministratoriService as= null;
 		try {
 			as = new DatiAmministratoriService();
 		} catch (DaoException e1) {
