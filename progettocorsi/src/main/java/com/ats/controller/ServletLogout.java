35a6e55433274bb2356464f6b904a5e42d057bb8
FrancescaMeroni <Administrator@192.168.12.52>
2019-11-27 16:41:55 +0100

    ServletLogout

--- progettocorsi/src/main/java/com/ats/controller/ServletLogout.java
+++ progettocorsi/src/main/java/com/ats/controller/ServletLogout.java
@@ -1,7 +1,6 @@
 package com.ats.controller;
 
 import java.io.IOException;
-import java.time.LocalDate;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,8 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.ats.exception.DaoException;
-import com.ats.service.DaoDatiCorsiImplService;
 
 public class ServletLogout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
